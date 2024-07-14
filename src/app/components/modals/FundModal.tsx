ca99032f11a59122af53ccf9007ea704f298ab0e
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-07-14 12:27:22 +0530

    Deposits

--- src/app/components/modals/FundModal.tsx
+++ src/app/components/modals/FundModal.tsx
@@ -28,6 +28,19 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 
+import {
+  StellarWalletsKit,
+  WalletNetwork,
+  type ISupportedWallet,
+  XBULL_ID,
+  xBullModule,
+  FreighterModule,
+  AlbedoModule,
+} from "@creit.tech/stellar-wallets-kit";
+import * as StellarSdk from "@stellar/stellar-sdk";
+import axios from "axios";
+import axiosInstance from "@/app/utils/apis";
+
 const FundModal = () => {
   const [userInputs, setUserInputs] = useState({
     email: "",
@@ -49,7 +62,85 @@ const FundModal = () => {
   const selectedCurrency = currencies.find(
     (currency) => currency.value === userInputs.token,
   );
-  console.log("currencies" + currencies);
+
+  const kit = new StellarWalletsKit({
+    selectedWalletId: XBULL_ID,
+    network: WalletNetwork.TESTNET,
+    modules: [new xBullModule(), new FreighterModule(), new AlbedoModule()],
+  });
+
+  const handleWalletSelection = async (option: ISupportedWallet) => {
+    console.log(option.isAvailable);
+
+    if (!option.isAvailable) {
+      // TODO: Show error
+      return;
+    }
+
+    kit.setWallet(option.id);
+
+    const publicKey = await kit.getPublicKey();
+
+    try {
+      const server = new StellarSdk.Horizon.Server(
+        "https://horizon-testnet.stellar.org",
+      );
+
+      // TODO: Check if account activated
+      const account = await server.loadAccount(publicKey);
+
+      // TODO: Put server wallet address in env
+      const transaction = new StellarSdk.TransactionBuilder(account, {
+        fee: StellarSdk.BASE_FEE,
+        networkPassphrase: StellarSdk.Networks.TESTNET,
+      })
+        .addOperation(
+          StellarSdk.Operation.payment({
+            destination:
+              "GDDX2B6TRVLQBUO3KBHO4EHRLPCCC5ZWEAEUXB3SBFEIE3YKXLF4AITV",
+            asset: StellarSdk.Asset.native(),
+            amount: userInputs.amount,
+          }),
+        )
+        .addMemo(StellarSdk.Memo.text("Deposite"))
+        .setTimeout(60)
+        .build();
+
+      const { result: signedTxnXdr } = await kit.signTx({
+        xdr: transaction.toXDR(),
+        publicKeys: [publicKey],
+        network: WalletNetwork.TESTNET,
+      });
+
+      const signedTxn = new StellarSdk.Transaction(
+        signedTxnXdr,
+        StellarSdk.Networks.TESTNET,
+      );
+
+      const txnResult = await server.submitTransaction(signedTxn, {
+        skipMemoRequiredCheck: true,
+      });
+
+      const verifyDeposit = await axiosInstance.post("/wallet/deposit", {
+        txHash: txnResult.hash,
+      });
+      console.log(verifyDeposit.data);
+
+      return txnResult.successful;
+    } catch (error: any) {
+      console.error(`Error depositing funds - ${error?.message}`);
+
+      return false;
+    }
+  };
+
+  const onClick = async () => {
+    await kit.openModal({
+      onWalletSelected: (option) => {
+        void handleWalletSelection(option);
+      },
+    });
+  };
 
   return (
     <div>
@@ -126,7 +217,7 @@ const FundModal = () => {
             </div>
           </div>
           <DialogFooter>
-            <Button type="button" className="mt-4 w-full">
+            <Button type="button" className="mt-4 w-full" onClick={onClick}>
               Deposit Funds
             </Button>
           </DialogFooter>
